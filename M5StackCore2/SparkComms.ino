ee754b25184cf053eaba100852ce318ebb564061
paulhamsh <p.w.hamshere@gmail.com>
2021-10-12 11:47:39 +0100

    Add files via upload

---
+++ M5StackCore2/SparkComms.ino
@@ -0,0 +1,467 @@
+#include "Spark.h"
+#include "SparkComms.h"
+#include "BluetoothSerial.h"
+
+
+
+// NEW CODE ------------------------------------------
+
+
+
+void notifyCB_sp(BLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify){
+  int i;
+  byte b;
+
+  Serial.print("Spark: ");
+  for (i = 0; i < length; i++) {
+    b = pData[i];
+    Serial.print(b, HEX);
+    Serial.print(" ");
+    ble_in.add(b);
+  }
+  Serial.println();
+  ble_in.commit();
+
+  // triggered_to_app = true;
+
+}
+
+
+void notifyCB_pedal(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify){
+  int i;
+  
+  Serial.print("Pedal: ");
+  for (i = 0; i < length; i++) {
+    Serial.print(pData[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
+
+  // In mode B the BB gives 0x80 0x80 0x90 0xNN 0x64 or 0x80 0x80 0x80 0xNN 0x00 for on and off
+  // In mode C the BB gives 0x80 0x80 0xB0 0xNN 0x7F or 0x80 0x80 0xB0 0xNN 0x00 for on and off
+
+  /*
+  if (pData[2] == 0x90 || (pData[2] == 0xB0 && pData[4] == 0x7F)) {
+    switch (pData[3]) {
+      case 0x3C:
+      case 0x14:
+        curr_preset = 0;
+        break;
+      case 0x3E:
+      case 0x15:
+        curr_preset = 1;
+        break;
+      case 0x40:
+      case 0x16:
+        curr_preset = 2;
+        break;
+      case 0x41:
+      case 0x17:
+        curr_preset = 3;
+        break;
+    }
+
+    // triggered_pedal = true;
+  }
+  */
+}
+
+/** Handler class for characteristic actions */
+class CharacteristicCallbacks: public NimBLECharacteristicCallbacks {
+    void onRead(NimBLECharacteristic* pCharacteristic){
+        int j, l;
+        const char *p;
+        byte b;
+        
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onRead, value: ");
+
+        l = pCharacteristic->getValue().length();
+        p = pCharacteristic->getValue().c_str();
+        for (j=0; j < l; j++) {
+          b = p[j];
+          Serial.print(" ");
+          Serial.print(b, HEX); 
+        };
+        Serial.println();
+    };
+
+    void onWrite(NimBLECharacteristic* pCharacteristic) {
+        Serial.print(pCharacteristic->getUUID().toString().c_str());
+        Serial.print(": onWrite(), value: ");
+      
+        int j, l;
+        const char *p;
+        byte b;
+        l = pCharacteristic->getValue().length();
+        p = pCharacteristic->getValue().c_str();
+        for (j=0; j < l; j++) {
+          b = p[j];
+          ble_app_in.add(b);
+          Serial.print(" ");
+          Serial.print(b, HEX); 
+//          pass_amp[j] = b;
+        }
+        Serial.println();
+        ble_app_in.commit();
+        
+//        pass_size_amp = j;
+        // triggered_to_amp = true;
+    };
+   
+    
+    /** Called before notification or indication is sent, 
+     *  the value can be changed here before sending if desired.
+     */
+    void onNotify(NimBLECharacteristic* pCharacteristic) {
+        Serial.println("Sending notification to clients");
+    };
+
+
+    /** The status returned in status is defined in NimBLECharacteristic.h.
+     *  The value returned in code is the NimBLE host return code.
+     */
+    void onStatus(NimBLECharacteristic* pCharacteristic, Status status, int code) {
+        String str = ("Notification/Indication status code: ");
+        str += status;
+        str += ", return code: ";
+        str += code;
+        str += ", "; 
+        str += NimBLEUtils::returnCodeToString(code);
+        Serial.println(str);
+    };
+
+    void onSubscribe(NimBLECharacteristic* pCharacteristic, ble_gap_conn_desc* desc, uint16_t subValue) {
+        String str = "Client ID: ";
+        str += desc->conn_handle;
+        str += " Address: ";
+        str += std::string(NimBLEAddress(desc->peer_ota_addr)).c_str();
+        if(subValue == 0) {
+            str += " Unsubscribed to ";
+        }else if(subValue == 1) {
+            str += " Subscribed to notfications for ";
+        } else if(subValue == 2) {
+            str += " Subscribed to indications for ";
+        } else if(subValue == 3) {
+            str += " Subscribed to notifications and indications for ";
+        }
+        str += std::string(pCharacteristic->getUUID()).c_str();
+
+        if (subValue == 1 && strcmp(std::string(pCharacteristic->getUUID()).c_str(), S_CHAR2) == 0) {
+          M5.Lcd.println("App active");
+          Serial.println("App active");
+        }
+
+        Serial.println(str);
+    };
+};
+
+static CharacteristicCallbacks chrCallbacks_s, chrCallbacks_r;
+
+bool connected_pedal, connected_sp;
+
+
+void connect_to_all(bool isBLE) {
+  int i;
+  is_ble = isBLE;
+  
+ // Create server to act as Spark
+  NimBLEDevice::init("Spark 40 BLE");
+  pClient_sp = NimBLEDevice::createClient();
+  pScan      = NimBLEDevice::getScan();
+    
+  // Set up server
+  pServer = NimBLEDevice::createServer();
+  pService = pServer->createService(S_SERVICE);
+  pCharacteristic_receive = pService->createCharacteristic(S_CHAR1, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::WRITE_NR);
+  pCharacteristic_send = pService->createCharacteristic(S_CHAR2, NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::NOTIFY);  
+  pCharacteristic_receive->setCallbacks(&chrCallbacks_r);
+  pCharacteristic_send->setCallbacks(&chrCallbacks_s);
+
+  pService->start();
+  pServer->start();
+
+  pAdvertising = NimBLEDevice::getAdvertising(); // create advertising instance
+  pAdvertising->addServiceUUID(pService->getUUID()); // tell advertising the UUID of our service
+  pAdvertising->setScanResponse(true);  
+
+  Serial.println("Service set up");
+
+  
+  // Connect to Spark
+  connected_sp = false;
+  connected_pedal = false;
+  
+  while (!connected_sp /* || !connected_pedal*/ ) {
+    pResults = pScan->start(4);
+    NimBLEUUID SpServiceUuid(C_SERVICE);
+    NimBLEUUID PedalServiceUuid(PEDAL_SERVICE);
+
+    Serial.println("------------------------------");
+    for(i = 0; i < pResults.getCount()  && (!connected_sp /* || !connected_pedal */); i++) {
+      device = pResults.getDevice(i);
+
+      // Print info
+      Serial.print("Name ");
+      Serial.println(device.getName().c_str());
+
+      if (device.isAdvertisingService(SpServiceUuid)) {
+        Serial.println("Found Spark - trying to connect....");
+        if(pClient_sp->connect(&device)) {
+          connected_sp = true;
+          Serial.println("Spark connected");
+          M5.Lcd.println("Spark connected");
+        }
+      }
+
+      if (strcmp(device.getName().c_str(),"iRig BlueBoard") == 0) {
+        Serial.println("Found pedal by name - trying to connect....");
+        pClient_pedal = NimBLEDevice::createClient();
+        if(pClient_pedal->connect(&device)) {
+          connected_pedal = true;
+          Serial.println("Pedal connected");
+          M5.Lcd.println("Pedal connected");
+        }
+      }
+    }
+
+    // Set up client
+    if (connected_sp) {
+      pService_sp = pClient_sp->getService(SpServiceUuid);
+      if (pService_sp != nullptr) {
+        pSender_sp   = pService_sp->getCharacteristic(C_CHAR1);
+        pReceiver_sp = pService_sp->getCharacteristic(C_CHAR2);
+        if (pReceiver_sp && pReceiver_sp->canNotify()) {
+          if (!pReceiver_sp->subscribe(true, notifyCB_sp, true)) {
+            connected_sp = false;
+            Serial.println("Spark disconnected");
+            //pClient_sp->disconnect();
+            NimBLEDevice::deleteClient(pClient_sp);
+          }
+        }
+      }
+    }
+
+    if (connected_pedal) {
+      pService_pedal = pClient_pedal->getService(PedalServiceUuid);
+      if (pService_pedal != nullptr) {
+        pReceiver_pedal = pService_pedal->getCharacteristic(PEDAL_CHAR);
+        if (pReceiver_pedal && pReceiver_pedal->canNotify()) {
+          if (!pReceiver_pedal->subscribe(true, notifyCB_pedal, true)) {
+            connected_pedal = false;
+            pClient_pedal->disconnect();
+          }
+        }
+      }
+    }
+
+  }
+
+  Serial.println("Available for app to connect...");
+  
+  // start advertising
+  pAdvertising->start(); 
+
+}
+
+// NEW CODE ENDS ------------------------------------------
+
+/*
+
+// Updated with changes from David Thompson github: https://github.com/happyhappysundays/SparkBoxHeltec - most of the BLE callbacks
+
+//BLE callbacks
+
+
+// Callbacks for client events
+class ClientCallbacks : public NimBLEClientCallbacks {
+  void onConnect(NimBLEClient* pClient) {
+    isBTConnected = true;
+  }
+
+  void onDisconnect(NimBLEClient* pClient) {
+    isBTConnected = false;
+  }
+};
+
+// Callbacks for advertisment events
+class AdvertisedDeviceCallbacks: public NimBLEAdvertisedDeviceCallbacks {
+  void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
+    if(advertisedDevice->isAdvertisingService(NimBLEUUID("ffc0")))
+    {
+      NimBLEDevice::getScan()->stop();
+    }
+  }
+};
+
+static ClientCallbacks clientCB;
+
+// Callback to process the results of the last scan or restart it 
+//void scanEndedCB(NimBLEScanResults results){
+//    DEBUG("Scan Ended");
+//}
+
+
+// Callback function to write to ring buffer
+void notifyCB(NimBLERemoteCharacteristic* pRemoteCharacteristic, uint8_t* pData, size_t length, bool isNotify){
+  int i;
+
+  for (i = 0; i < length; i++) 
+    ble_in.add(pData[i]);
+
+  ble_in.commit();
+}
+
+
+
+
+// Non callback functions
+void start_ser() {
+  uint8_t b;
+  
+  ser = new HardwareSerial(2); 
+  // 5 is rx, 18 is tx
+  ser->begin(HW_BAUD, SERIAL_8N1, 5, 18);
+
+  while (ser->available())
+    b = ser->read(); 
+}
+
+void start_bt(bool isBLE) {
+  is_ble = isBLE;
+  if (!is_ble) {
+    bt = new BluetoothSerial();
+  
+    if (!bt->begin (MY_NAME, true)) {
+      DEBUG("Bluetooth init fail");
+      while (true);
+    }    
+  }
+  else {
+    NimBLEDevice::init("");
+  }
+}
+
+void connect_to_spark() {
+  uint8_t b;
+  int i;
+  bool connected;
+
+  connected = false;
+
+  if (!is_ble) {
+    while (!connected) {
+      connected = bt->connect(SPARK_NAME);
+      if (!(connected && bt->hasClient())) {
+        connected = false;
+        DEBUG("Not connected");
+        delay(2000);
+      }
+    }
+
+    // flush anything read from Spark - just in case
+    while (bt->available())
+      b = bt->read(); 
+  }
+  else {
+    while (!connected) {
+      NimBLEDevice::init("");
+  
+      NimBLEScan *pScan = NimBLEDevice::getScan();
+      
+      // Create a callback that gets called when advertisers are found 
+      pScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+      NimBLEScanResults results = pScan->start(4);
+
+      NimBLEUUID serviceUuid("ffc0");               // service ffc0 for Spark
+
+      for(i = 0; i < results.getCount() && !connected; i++) {
+        device = results.getDevice(i);
+    
+        if (device.isAdvertisingService(serviceUuid)) {
+          pClient = NimBLEDevice::createClient();
+          pClient->setClientCallbacks(&clientCB, false);
+        
+          if(pClient->connect(&device)) {
+            connected = true;
+            DEBUG("BLE Connected");
+          }
+        }
+      }
+
+      // Get the services
+  
+      if (connected) {
+        pService = pClient->getService(serviceUuid);
+                
+        if (pService != nullptr) {
+          pSender   = pService->getCharacteristic("ffc1");
+          pReceiver = pService->getCharacteristic("ffc2");
+          if (pReceiver && pReceiver->canNotify()) {
+            if (!pReceiver->subscribe(true, notifyCB, true)) {
+              connected = false;
+              pClient->disconnect();
+              DEBUG("BLE not connected - could not subscribe to callback");
+            }
+          }
+        }
+      }
+      
+      if (!connected) {
+        DEBUG ("Not connected - trying again");
+        delay(200); // Idea from https://github.com/espressif/esp-idf/issues/5105
+      }
+    }
+  }
+}
+
+*/
+
+bool ser_available() {
+  // return ser->available();
+  return !ble_app_in.is_empty();
+}
+
+bool bt_available() {
+  if (!is_ble) {
+    return bt->available();
+  }
+  else {
+    return !ble_in.is_empty();
+  }
+}
+
+uint8_t ser_read() {
+  //return ser->read();
+  uint8_t b;
+  ble_app_in.get(&b);
+  return b;
+}
+
+uint8_t bt_read() {
+  if (!is_ble) {
+    return bt->read();
+  }
+  else {
+    uint8_t b;
+    ble_in.get(&b);
+    return b;
+  }
+}
+
+void ser_write(byte *buf, int len) {
+//  ser->write(buf, len);
+  pCharacteristic_send->setValue(buf, len);
+  pCharacteristic_send->notify(true);
+}
+
+void bt_write(byte *buf, int len) {
+  if (!is_ble) 
+    bt->write(buf, len);
+  else 
+    pSender_sp->writeValue(buf, len, false);
+}
+
+int ble_getRSSI() {
+  return pClient_sp->getRssi();
+}
---
+++ M5StackCore2/SparkIO.h
@@ -0,0 +1,144 @@
+#ifndef SparkIO_h
+#define SparkIO_h
+
+#include "RingBuffer.h"
+#include "Spark.h"
+#include "SparkComms.h"
+
+#define MAX_BT_BUFFER 5000
+
+class SparkIO
+{
+  public:
+    SparkIO(bool passthru);
+    ~SparkIO();
+
+    // SPARK MESSAGES
+
+    // overall processing
+
+    void sp_process();
+
+    //  receiving data   
+    
+    void sp_process_in_blocks();
+    void sp_process_in_chunks();
+
+    void sp_process_out_chunks();
+    void sp_process_out_blocks();
+
+    // processing received messages    
+    bool sp_get_message(unsigned int *cmdsub, SparkMessage *msg, SparkPreset *preset);
+    // creating messages to send
+
+    void sp_create_preset(SparkPreset *preset);
+
+    void sp_turn_effect_onoff(char *pedal, bool onoff);
+    void sp_change_hardware_preset(uint8_t preset_num);
+    void sp_change_effect(char *pedal1, char *pedal2);
+    void sp_change_effect_parameter(char *pedal, int param, float val);
+
+    void sp_get_serial();
+    void sp_get_name();
+    void sp_get_hardware_preset_number();
+    void sp_get_preset_details(unsigned int preset);
+    
+    // sending data
+
+    void sp_out_store(uint8_t b);
+
+    // chunk variables (read from bluetooth into a chunk ring buffer)
+    // in_chunk.is_empty() is false when there is data to read
+
+    RingBuffer sp_in_chunk;
+    int sp_rb_state;
+    int sp_rb_len;
+
+    // message variables (read from chunk read buffer into in_message store - a single message
+    // in_mesage_ready is true when there is a full message to read
+
+    RingBuffer sp_in_message;
+    int sp_rc_state;
+    bool sp_in_message_bad;
+    
+    int sp_rc_seq;
+    int sp_rc_cmd;
+    int sp_rc_sub;
+    int sp_rc_checksum;
+    
+    int sp_rc_calc_checksum;
+
+    bool sp_rc_multi_chunk;
+    int sp_rc_data_pos;
+    uint8_t sp_rc_bitmask;
+    int sp_rc_bits;
+
+    int sp_rc_total_chunks;
+    int sp_rc_this_chunk;
+    int sp_rc_chunk_len;
+    int sp_rc_last_chunk;
+
+    int sp_rd_pos;
+    
+    // message variables for sending
+
+    // out_message
+    
+    RingBuffer sp_out_message;
+    int sp_om_cmd;
+    int sp_om_sub;
+    int sp_out_msg_chksum;
+
+    // out_chunk
+
+    RingBuffer sp_out_chunk;
+    uint8_t sp_oc_seq;
+    uint8_t sp_oc_cmd;
+    uint8_t sp_oc_sub;
+    unsigned int sp_oc_len;
+
+    uint8_t sp_oc_bit_mask;
+    int sp_oc_bit_pos;
+    uint8_t sp_oc_checksum;
+
+    // out_block
+    uint8_t sp_out_block[0xad];
+    int sp_ob_pos;
+    bool sp_ob_ok_to_send;
+    uint8_t sp_ob_last_seq_sent;
+    unsigned int sp_ob_last_sent_time;
+
+        // passthrough
+
+    uint8_t sp_bt_buf[MAX_BT_BUFFER];
+    int sp_bt_pos;
+    int sp_bt_len;
+    int sp_bt_state;
+    bool sp_pass_through;
+    
+  private:
+   
+    // routines to read the msgpack data
+    void sp_read_string(char *str);
+    void sp_read_prefixed_string(char *str);
+    void sp_read_onoff(bool *b);
+    void sp_read_float(float *f);
+    void sp_read_byte(uint8_t *b);
+
+    // routines to write the msgfmt data
+    void sp_start_message(int cmdsub);
+    void sp_end_message();
+    void sp_write_byte(byte b);
+    void sp_write_byte_no_chksum(byte b);
+    void sp_write_prefixed_string(const char *str);
+    void sp_write_long_string(const char *str);
+    void sp_write_string(const char *str);
+    void sp_write_float(float flt);
+    void sp_write_onoff(bool onoff);
+
+
+
+};
+
+#endif
+      
